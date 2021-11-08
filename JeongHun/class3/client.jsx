efc1b4b45cd5a3096fd08007d1aaacb1945227bc
JeongHunMoon <gkrry9409@gmail.com>
2021-11-08 15:33:55 +0900

    class3_BaseBallGame

---
+++ JeongHun/class3/client.jsx
@@ -0,0 +1,5 @@
+const React = require('react');
+const ReactDom = require('react-dom');
+import BaseBallGame from './BaseBallGame.jsx';
+
+ReactDom.render(<React.StrictMode><BaseBallGame /></React.StrictMode>, document.querySelector('#root'));
