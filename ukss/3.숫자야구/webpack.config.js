f70032ed670e84b4f41d9c1037e9876cdd2c0431
ukssss <1872003115su@gmail.com>
2021-11-12 17:42:59 +0900

    [ukss] Git test in mac

---
+++ ukss/3.숫자야구/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    devtool: 'eval',
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+
+    entry: {
+        app: ['./client']
+    },
+
+    module: {
+        rules: [{
+            test: /\.jsx?$/,
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env', '@babel/preset-react'],
+                plugins: ['@babel/plugin-proposal-class-properties']
+            },
+        }]
+    },
+
+    output: {
+        filename: 'app.js',
+        path: path.join(__dirname, 'dist')
+    },
+
+};
